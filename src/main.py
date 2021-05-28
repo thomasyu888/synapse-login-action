b32c6a88820d65c359c4fae4265298dc3fb53775
thomasyu888 <thomas.yu@sagebase.org>
2021-05-28 23:33:03 +0800

    Add tests and support for PAT

--- src/main.py
+++ src/main.py
@@ -4,7 +4,15 @@ import synapseclient
 
 username = os.getenv("INPUT_USERNAME")
 apikey = os.getenv("INPUT_APIKEY")
+pat = os.getenv("INPUT_PAT")
 
-syn = synapseclient.login(email=username,
-                          apiKey=apikey,
-                          rememberMe=True)
+if apikey is None and pat is None:
+    raise ValueError("Must specify pat or apikey")
+
+if pat is not None:
+    syn = synapseclient.login(authToken=pat,
+                              rememberMe=True)
+else:
+    syn = synapseclient.login(email=username,
+                              apiKey=apikey,
+                              rememberMe=True)
