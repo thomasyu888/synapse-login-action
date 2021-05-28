af908eedcb0df8f15e60a6de529596401a114e7d
thomasyu888 <thomas.yu@sagebase.org>
2021-05-28 23:51:44 +0800

    Fix

--- src/main.py
+++ src/main.py
@@ -5,12 +5,11 @@ import synapseclient
 username = os.getenv("INPUT_USERNAME")
 apikey = os.getenv("INPUT_APIKEY")
 pat = os.getenv("INPUT_PAT")
-print(pat)
 
 if apikey == '' and pat is '':
     raise ValueError("Must specify pat or apikey")
 
-if pat is not None:
+if pat != '':
     syn = synapseclient.login(authToken=pat,
                               rememberMe=True)
 else:
